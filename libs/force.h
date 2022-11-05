b5a2a9b7547914e287bbe55ce5ff4a6aa50149d6
INorthDev <canaykacahmet@gmail.com>
2022-11-06 00:58:56 +0300

    Update: -Vector subtraction fixed

---
+++ libs/force.h
@@ -0,0 +1,15 @@
+//
+// Created by ahmet on 05.11.2022.
+//
+
+#ifndef INC_2DPHYSICSENGINE1_FORCE_H
+#define INC_2DPHYSICSENGINE1_FORCE_H
+#include "vector2d.h"
+
+class Force : Vector2D<float> {
+public:
+    float x;
+    float y;
+};
+
+#endif //INC_2DPHYSICSENGINE1_FORCE_H
